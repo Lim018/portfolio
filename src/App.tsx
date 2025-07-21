0efa56f175162fea9435dec1ca0cc1407e2deb0e
alim <149599272+Lim018@users.noreply.github.com>
2025-07-21 10:40:50 +0700

    fix react depen

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react"
+import { useEffect, useRef } from "react"
 import Navigation from "./components/Navigation"
 import ScrollProgress from "./components/ScrollProgress"
 import HeroSection from "./components/HeroSection"
